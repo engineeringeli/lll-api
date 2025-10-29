408fa3c08d7037f859e5a8449f10762a7e0bf1e8
engineeringeli <thatphotoguywill@gmail.com>
2025-10-29 06:10:19 -0700

    Update routes_debug.py

--- app/routes_debug.py
+++ app/routes_debug.py
@@ -1,12 +1,12 @@
 # backend/app/routes_debug.py
 from fastapi import APIRouter, Depends
 from psycopg import Connection
-from app.deps import get_db
+from app.deps import db_conn
 
 router = APIRouter(prefix="/debug", tags=["debug"])
 
 @router.get("/db")
-def debug_db(db: Connection = Depends(get_db)):
+def debug_db(db: Connection = Depends(db_conn)):
     info = db.execute("""
         select
           current_user,
